49f8d311a67a8270f383eb6c60dca90ab199a79d
Madfarm <anthony.scott1005@gmail.com>
2023-08-15 22:32:47 -0400

    Setup DbSets in our DbContext

--- FirstVSApi/Data/DataContext.cs
+++ FirstVSApi/Data/DataContext.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using FirstVSApi.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace FirstVSApi.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-  
+        public DbSet<Dishes> DishesTable { get; set; }
+        public DbSet<Ingredient> IngredientsTable { get; set; }
     }
 }
